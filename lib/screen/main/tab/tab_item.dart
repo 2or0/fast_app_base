b5c9bf9c5678e6bb884c671261f1d0daaab3d514
2or0 <withtbl@naver.com>
2025-01-15 01:04:01 +0900

    init

--- lib/screen/main/tab/tab_item.dart
+++ lib/screen/main/tab/tab_item.dart
@@ -1,11 +1,18 @@
 import 'package:fast_app_base/common/common.dart';
-import 'package:fast_app_base/screen/main/tab/favorite/f_favorite.dart';
 import 'package:fast_app_base/screen/main/tab/home/f_home.dart';
 import 'package:flutter/material.dart';
 
+import 'all/f_all.dart';
+import 'benefit/f_benefit.dart';
+import 'pay/f_pay.dart';
+import 'stock/f_pay.dart';
+
 enum TabItem {
   home(Icons.home, '홈', HomeFragment()),
-  favorite(Icons.star, '즐겨찾기', FavoriteFragment(isShowBackButton: false));
+  benefit(Icons.star, '혜택', BenefitFragment()),
+  pay(Icons.payment, '토스페이', PayFragment()),
+  stock(Icons.candlestick_chart, '주식', StockFragment()),
+  all(Icons.menu, '전체', AllFragment());
 
   final IconData activeIcon;
   final IconData inActiveIcon;
@@ -14,7 +21,9 @@ enum TabItem {
 
   const TabItem(this.activeIcon, this.tabName, this.firstPage, {IconData? inActiveIcon})
       : inActiveIcon = inActiveIcon ?? activeIcon;
+}
 
+extension TabItemExtension on TabItem {
   BottomNavigationBarItem toNavigationBarItem(BuildContext context, {required bool isActivated}) {
     return BottomNavigationBarItem(
         icon: Icon(
